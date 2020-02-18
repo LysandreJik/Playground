92366f9074b349d7408910f9b33b812c7eebbcc6
Lysandre Debut <lysandre.debut@reseau.eseo.fr>
2020-02-17 22:22:40 -0500

    JAX Lib + example

--- jax/GettingStarted/main.py
+++ jax/GettingStarted/main.py
@@ -1,4 +1,17 @@
+from jax import grad
+import jax.numpy as np
+
+
+def tanh(x):
+	y = np.exp(-2.0 * x)
+	return (1.0 - y) / (1.0 + y)
+
+
+def main():
+	grad_tanh = grad(tanh)
+	print(grad_tanh(1.0))
 
 
 if __name__ == "__main__":
 	print("JAX playground")
+	main()
